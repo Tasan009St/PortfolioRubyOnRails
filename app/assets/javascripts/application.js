e75d249ab9deba936dc311f27a44a85943071152
Tasan009St <orangebbumesak@gmail.com>
2018-12-12 21:22:34 +0900

    corrected application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require rails-ujs
 //= require bootstrap
+//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require jquery3
-//= require bootstrap-sprockets
+//= require jquery
